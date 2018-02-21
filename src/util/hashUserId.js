a52f59f95249bb882258038df1093c6afa5a0be9
Knut Melvær <knut.melvaer@gmail.com>
2018-02-21 13:46:44 +0100

    fix object path
    `userIdHashSalt` is placed under the `sanitySession`-key in the config object

--- src/util/hashUserId.js
+++ src/util/hashUserId.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 const config = require('../../config')
 
 module.exports = function hashUserId(username) {
-  return crypto.createHmac('sha256', config.userIdHashSalt)
+  return crypto.createHmac('sha256', config.sanitySession.userIdHashSalt)
     .update(username.toLowerCase())
     .digest('hex')
 }
